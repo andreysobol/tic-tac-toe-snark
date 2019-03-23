4481e2d5d0baf35ae14d92849b7c0acbbf77a8c7
Andrey Sobol <baskerville@andreysobol.com>
2019-03-23 17:16:53 +0200

    pweqpl to gen

--- generator.py
+++ generator.py
@@ -1,8 +1,16 @@
-template = open("pweqpw.template").read()
+pweqpw_template = open("pweqpw.template").read()
+pweqpl_template = open("pweqpl.template").read()
+
 result = ""
+
 for item in range(0,4):
-    tr = template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
+    tr = pweqpw_template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
     result = result + tr
+
+for item in range(0,5):
+    tr = pweqpl_template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
+    result = result + tr
+
 compiled = open("ticktactoe.compiled.code", 'w')
 compiled.write(result)
 compiled.close()
