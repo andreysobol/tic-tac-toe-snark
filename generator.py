f2cbf0f05f12516cb4bc35077826c11ec07dfe63
Andrey Sobol <baskerville@andreysobol.com>
2019-03-23 17:07:06 +0200

    pweqpw.template

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
-template = open("template").read()
+template = open("pweqpw.template").read()
 result = ""
-for item in range(0,3):
+for item in range(0,4):
     tr = template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
     result = result + tr
 compiled = open("ticktactoe.compiled.code", 'w')
