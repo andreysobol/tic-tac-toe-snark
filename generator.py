462f781bd04b75e31a53a97eec7901f344f34ef6
Andrey Sobol <baskerville@andreysobol.com>
2019-03-23 18:27:01 +0200

    content in template

--- generator.py
+++ generator.py
@@ -1,15 +1,18 @@
 pweqpw_template = open("pweqpw.template").read()
 pweqpl_template = open("pweqpl.template").read()
+tictactoe_template = open("tictactoe.template").read()
 
-result = ""
+content = ""
 
 for item in range(0,4):
     tr = pweqpw_template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
-    result = result + tr
+    content = content + tr
 
 for item in range(0,5):
     tr = pweqpl_template.replace('{{index}}', str(item)).replace('{{indexp1}}', str(item+1))
-    result = result + tr
+    content = content + tr
+
+result = tictactoe_template.replace('{{content}}', content)
 
 compiled = open("ticktactoe.compiled.code", 'w')
 compiled.write(result)
